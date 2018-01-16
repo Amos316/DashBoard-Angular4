ed3071d10cf88a98da4c311e8b28b8235882eace
Elisha Amos <kwekuamos316@hotmail.com>
2018-01-16 01:17:12 -0500

    DashBoard App using Firebase via AngularFire and Angular Flash Messaging.

---
+++ src/app/components/client-details/client-details.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { ClientService } from '../../services/client.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { Client } from '../../models/Client';
+
+@Component({
+  selector: 'app-client-details',
+  templateUrl: './client-details.component.html',
+  styleUrls: ['./client-details.component.css']
+})
+export class ClientDetailsComponent implements OnInit {
+      id: string;
+      client: Client;
+      hasBalance: boolean = false;
+      showBalanceUpdateInput: boolean = false;
+
+  constructor(private clientService: ClientService, private router: Router,
+              private route: ActivatedRoute, private flashMessagesService: FlashMessagesService) { }
+
+  ngOnInit() {
+    this.id = this.route.snapshot.params['id'];
+    this.clientService.getClient(this.id).subscribe(client => {
+        if(client.balance > 0){
+            this.hasBalance = true;
+        }
+            this.client = client;
+    });
+  }
+    updateBalance(id: string){
+      this.clientService.updateClient(this.id, this.client);
+      this.flashMessagesService.show('Balanced Updated', {
+        cssClass: 'alert-success', timeout: 4000
+      });
+      this.router.navigate(['/client/'+this.id]);
+    }
+    onDeleteClick(){
+      if(confirm("Confirm you want to delete client")){
+        this.clientService.deleteClient(this.id);
+        this.flashMessagesService.show('Client removed', {
+          cssClass: 'alert-success', timeout: 4000
+        });
+        this.router.navigate(['/']);
+      }
+    }
+  
+  }
---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCPK60JZzctMZK16ID9xgIE_1h8oa-qDCI",
+    authDomain: "clientpanel-db3db.firebaseapp.com",
+    databaseURL: "https://clientpanel-db3db.firebaseio.com",
+    projectId: "clientpanel-db3db",
+    storageBucket: "clientpanel-db3db.appspot.com",
+    messagingSenderId: "571450726706"
+  }
+};
